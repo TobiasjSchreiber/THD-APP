678601516a6bb0b65c80e305a1b8e11bc9bab8f6
Tobias <tobiasjschreiber@gmail.com>
2026-03-31 19:58:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'thd-app-v4';
+const ASSETS = [
+  './THD_APP.html',
+  './manifest.json',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Installiert');
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(err => console.log('Cache Fehler:', err));
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('[Service Worker] Aktiviert');
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
